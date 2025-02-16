b839fac6ef994b8b27bc0f45fd98d94dd0206e6c
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-02-16 19:58:55 +0200

    add task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,18 @@
-'use strict';
+const input = document.getElementById('name-input');
+const span_output = document.getElementById('name-output');
 
-function getElementWidth(content, padding, border) {
-  const content_size = parseFloat(content);
-  const padding_size = parseFloat(padding) * 2;
-  const border_size = parseFloat(border) * 2;
-  return content_size + padding_size + border_size;
+input.style.cssText = `
+    width: 360px;
+    padding: 8px 16px;
+    border-radius: 4px;
+    border: 1px solid rgb(128, 128, 128);`;
+
+input.addEventListener('input', change_user_name_hanlder);
+
+function change_user_name_hanlder(event) {
+  const default_name = 'Anonymous';
+  span_output.textContent = event.target.value;
+  if (event.target.value == '') {
+    span_output.textContent = default_name;
+  }
 }
-
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
