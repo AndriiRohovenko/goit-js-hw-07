bba534615d1d90cb37c271cb0e4f61679c1111f8
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-02-16 18:16:29 +0200

    add task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,39 @@
-'use strict';
+const list = document.querySelectorAll('ul');
+const categories = document.querySelectorAll('.item');
+const items = document.querySelectorAll('.item ul > li');
 
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = pricePerDroid * quantity;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
+const category_count_str = `Number of categories: ${categories.length}`;
+console.log(category_count_str);
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+list.forEach(el => {
+  el.style.listStyle = 'none';
+  el.style.padding = 0;
+  el.style.margin = 0;
+});
+
+categories.forEach(el => {
+  el.style.cssText = `
+      background-color: rgb(246, 246, 254);
+      border-radius: 8px;
+      margin-bottom: 24px;
+      padding: 16px;
+      width: 392px;
+    `;
+  const category_count_str = `Category: ${el.firstElementChild.textContent}`;
+  console.log(category_count_str);
+  const item_count_str = `Elements: ${el.lastElementChild.childElementCount}`;
+  console.log(item_count_str);
+});
+
+items.forEach(el => {
+  el.style.cssText = `
+    padding: 8px 16px;
+    margin-bottom: 8px;
+    border: 1px solid rgb(128, 128, 128);
+    border-radius: 4px;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.33;
+    color: rgb(46, 47, 66);
+      `;
+});
